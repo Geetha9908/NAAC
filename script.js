8e6971616cc29513aaf67c22d1dd6e34c32c4548
Geetha9908 <beerannagarigeetha202@gmail.com>
2025-12-28 20:54:50 +0530

    add a script.js as a test
    adding script file

--- script.js
+++ script.js
@@ -12,3 +12,24 @@ function sendMessage() {
 
   chatbox.innerHTML += "<p><b>Bot:</b> " + reply + "</p>";
 }
+else if (input.toLowerCase().includes("criteria 1")) {
+  reply = "Criteria 1: Curricular Aspects focuses on curriculum design and delivery.";
+}
+else if (input.toLowerCase().includes("criteria 2")) {
+  reply = "Criteria 2: Teaching-Learning and Evaluation.";
+}
+else if (input.toLowerCase().includes("criteria 3")) {
+  reply = "Criteria 3: Research, Innovations and Extension.";
+}
+else if (input.toLowerCase().includes("criteria 4")) {
+  reply = "Criteria 4: Infrastructure and Learning Resources.";
+}
+else if (input.toLowerCase().includes("criteria 5")) {
+  reply = "Criteria 5: Student Support and Progression.";
+}
+else if (input.toLowerCase().includes("criteria 6")) {
+  reply = "Criteria 6: Governance, Leadership and Management.";
+}
+else if (input.toLowerCase().includes("criteria 7")) {
+  reply = "Criteria 7: Institutional Values and Best Practices.";
+}
